014c2fb684fb8de475ff119ffc60a376832125be
Rashika <agarwalrashika00@gmail.com>
2023-06-22 15:53:12 +0530

    description can be max of 5 words

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,12 @@ class Project < ApplicationRecord
   has_many :developers, through: :employees_projects, source: :employee
   has_and_belongs_to_many :technologies
 
+  validates :description_words, length: {maximum: 5}
+
+  private
+
+  def description_words
+    description&.split
+  end
+
 end
